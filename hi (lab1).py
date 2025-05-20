98679915037835e71cca83311c355bbfe00b811c
Reson88 <54431700+Reson88@users.noreply.github.com>
2025-05-19 20:40:34 -0400

    Add files via upload

--- hi (lab1).py
+++ hi (lab1).py
@@ -1,6 +1,7 @@
+# This is certainly one of the programs ever :3 
 
-print("Hello, World!")
 
 
-print("mmm... burger king")
+print("Hello, World!") #prints saluton mondo
 
+print("mmm... burger king") #prints brgr 
